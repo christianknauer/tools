4a36e6899c62f6b408df9c55ebe8496b47b14851
Christian Knauer <email@christianknauer.de>
2023-09-10 21:08:41 +0200

    Use of submodule.

---
+++ do/lib.inc.sh
@@ -0,0 +1,8 @@
+# file: tools.inc.sh
+
+LIB_DIRECTORY=$(pwd)/../lib/bash
+
+export LOGGING_LEVEL_SCOPE="testing"
+source ${LIB_DIRECTORY}/logging.inc.sh
+ 
+# EOF
--- do/show_droplets.sh
+++ do/show_droplets.sh
@@ -2,7 +2,10 @@
 
 # show_droplets.sh
 
+source lib.inc.sh
 source token.inc.sh
+
+Begin "Starting"
 
 DROPLETS=$(curl -sS -X GET "https://api.digitalocean.com/v2/droplets" \
 	-H "Authorization: Bearer $DO_TOKEN")
