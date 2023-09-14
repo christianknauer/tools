3bf1e7682f9298d06745765fac1dbe9a15f925e7
Christian Knauer <email@christianknauer.de>
2023-09-14 14:31:03 +0200

    Work on ssh-agent encryption.

--- ssh/sa-encrypt.sh
+++ ssh/sa-encrypt.sh
@@ -21,13 +21,13 @@ Options.ParseOptions "${USAGE}" ${@}
 DebugLoggingConfig 9
 
 # check binaries
-ARCH=$(uname -p)
+ARCH=$(uname -m)
 CODE_DIR="$(dirname $0)/exec/${ARCH}"
 ENCRYPT="${CODE_DIR}/sshcrypt-agent-encrypt"
 DECRYPT="${CODE_DIR}/sshcrypt-agent-decrypt"
 [ ! -e "$ENCRYPT" ] && ErrorMsg "exec file \"${ENCRYPT}\" does not exist" && exit 1
 [ ! -e "$DECRYPT" ] && ErrorMsg "exec file \"${DECRYPT}\" does not exist" && exit 1
-DebugMsg 1 "using exec files \"${ENCRYPT}\", \"${DECRYPT}\""
+DebugMsg 3 "using exec files \"${ENCRYPT}\", \"${DECRYPT}\""
 
 # main
 
@@ -42,11 +42,11 @@ TEMPD=$(mktemp -d)
 
 # check if the temp directory was created successfully.
 [ ! -e "$TEMPD" ] && ErrorMsg "failed to create temporary directory" && exit 1
-DebugMsg 2 "created temporary directory $TEMPD"
+DebugMsg 3 "created temporary directory $TEMPD"
 
 # make sure the temp directory gets removed on script exit.
 trap "exit 1" HUP INT PIPE QUIT TERM
-trap 'DebugMsg 2 "removing temporary directory $TEMPD"; rm -rf "$TEMPD"'  EXIT
+trap 'DebugMsg 3 "removing temporary directory $TEMPD"; rm -rf "$TEMPD"'  EXIT
 
 # make sure the temp directory is in /tmp.
 [[ ! "$TEMPD" = /tmp/* ]] && ErrorMsg "temporary directory not in /tmp" && exit 1
@@ -58,20 +58,21 @@ trap 'DebugMsg 2 "removing temporary directory $TEMPD"; rm -rf "$TEMPD"'  EXIT
 DESTKEY="unspecified"
 if [ ! "${PUBKEYFILE}" == "" ]; then
 	[ ! -e "$PUBKEYFILE" ] && ErrorMsg "public key file \"$PUBKEYFILE\" cannot be opened" && exit 1
-	DebugMsg 1 "reading public key from \"$PUBKEYFILE\""
+	DebugMsg 3 "reading public key from \"$PUBKEYFILE\""
 	# read first line of file
 	read KeyType RestOfLine < ${PUBKEYFILE} 
         PublicKey=${RestOfLine%% *}
         KeyIDShort=${PublicKey: -8:8}
         if [[ $KeyType = ssh-rsa ]]; then
-	    InfoMsg "using public key ($KeyIDShort) from \"$PUBKEYFILE\""
+	    DebugMsg 1 "reading public key from \"$PUBKEYFILE\""
+	    DebugMsg 3 "using public key $KeyIDShort"
 	    DESTKEY=$PublicKey
         else 
 	    ErrorMsg "public key ($KeyIDShort) from \"$PUBKEYFILE\" is not an RSA key" && exit 1
         fi
 fi
 
-DebugMsg 1 "reading from \"$INFILE\", writing to \"$OUTFILE.*\""
+DebugMsg 3 "reading from \"$INFILE\", writing to \"$OUTFILE.*\""
 
 [ ! -e "$INFILE" ] && ErrorMsg "input file \"$INFILE\" cannot be opened" && exit 1
 
@@ -80,7 +81,7 @@ ENCFILE=$(mktemp -p $TEMPD)
 [ ! -e "$KEYFILE" ] && ErrorMsg "failed to create temporary key file" && exit 1
 [ ! -e "$ENCFILE" ] && ErrorMsg "failed to create temporary enc file" && exit 1
 
-DebugMsg 1 "using \"$KEYFILE\" as temp key file, \"$ENCFILE\" as temp enc file"
+DebugMsg 3 "using \"$KEYFILE\" as temp key file, \"$ENCFILE\" as temp enc file"
 
 ssh-add -L > ${KEYFILE} ; ec=$?  # grab the exit code into a variable so that it can
                                  # be reused later, without the fear of being overwritten
@@ -88,7 +89,7 @@ ssh-add -L > ${KEYFILE} ; ec=$?  # grab the exit code into a variable so that it
 NROFKEYS=$(cat ${KEYFILE} | wc -l)
 
 case $ec in
-    0) DebugMsg 1 "agent provides ${NROFKEYS} key(s)"; cat ${INFILE} | ${ENCRYPT} > ${ENCFILE};;
+    0) DebugMsg 3 "agent provides ${NROFKEYS} key(s)"; cat ${INFILE} | ${ENCRYPT} > ${ENCFILE};;
     1) ErrorMsg "ssh-agent has no identities ($ec)"; exit 1;;
     2) ErrorMsg "ssh-agent is not running ($ec)"; exit 2;;
     *) ErrorMsg "ssh-agent gives unknown exit code ($ec)"; exit 2;;
@@ -119,16 +120,18 @@ while IFS='' read -r LinefromFile || [[ -n "${LinefromFile}" ]]; do
     if [[ $KeyType = ssh-rsa ]]; then
             if [[ ${DESTKEY} == "unspecified" || ${PublicKey} = ${DESTKEY} ]]; then
                 ((RSACounter++))
-		EncOutputFile="${OUTFILE}.${KeyIDShort}"
-		InfoMsg "key #$Counter ($KeyIDShort) is accepted (result written to \"$EncOutputFile\")" #: ${LinefromFile}"
+		EncOutputFile="${TEMPD}/${OUTFILE}.${KeyIDShort}"
+		DebugMsg 1 "secret key found in agent"
+		DebugMsg 3 "key #$Counter ($KeyIDShort) is accepted, result written to \"$EncOutputFile\")" #: ${LinefromFile}"
 	        mv "${ENCFILE}.${Counter}" "$EncOutputFile"
 	        [ ! -e "$EncOutputFile" ] && ErrorMsg "failed to create output file \"$EncOutputFile\"" && exit 1
-	        chmod go-rwx "${OUTFILE}.${KeyIDShort}"
+	        chmod go-rwx "${EncOutputFile}"
+		break
 	    else
-	        DebugMsg 1 "key #$Counter ($KeyIDShort) is rejected (not the destination key)" #: ${LinefromFile}"
+	        DebugMsg 3 "key #$Counter ($KeyIDShort) is rejected (not the destination key)" 
 	    fi
     else 
-	WarnMsg "key #$Counter ($KeyIDShort) is ignored (no RSA key)" #: ${LinefromFile}"
+	DebugMsg 2 "key #$Counter ($KeyIDShort) is ignored (no RSA key)" #: ${LinefromFile}"
     fi
 done < "${KEYFILE}"
 
@@ -136,26 +139,33 @@ done < "${KEYFILE}"
 if [ ! "${INFILE}" == "/dev/stdin" ]; then
     VERFILE=$(mktemp -p $TEMPD)
     [ ! -e "$VERFILE" ] && ErrorMsg "failed to create temporary verification file" && exit 1
-    DebugMsg 1 "verifying encryption (temp file is \"$VERFILE\")"
+    DebugMsg 3 "verifying encryption (temp file is \"$VERFILE\")"
     cat "${EncOutputFile}" | ${DECRYPT} > "${VERFILE}"
     cmp -s "$INFILE" "${VERFILE}" ; ec=$?  
     case $ec in
-       0) InfoMsg "verification passed";;
+       0) DebugMsg 1 "verification passed";;
        *) ErrorMsg "verification failed" && exit 1;;
     esac
 else
-    WarnMsg "reading from stdin - verification skipped"
+    DebugMsg 2 "reading from stdin - verification skipped"
 fi
 
 if (( $RSACounter > 0 )); then
-    DebugMsg 1 "accepted $RSACounter key(s)"
+    DebugMsg 3 "accepted $RSACounter key(s)"
     if (( $RSACounter  == 1 )); then
-        InfoMsg "accepted unique key"
-	mv "$EncOutputFile" "$OUTFILE"
+	if [ "${INFILE}" == "/dev/stdin" ]; then
+	    cat "$EncOutputFile" 
+	    DebugMsg 1 "output sent to STDOUT"
+	else
+	    mv "$EncOutputFile" "$OUTFILE"
+	    DebugMsg 1 "output written to \"${OUTFILE}\""
+	fi
+    else
+	ErrorMsg "only one key should be accepted"; exit 1
     fi
     exit 0
 else
-    WarnMsg "no keys accepted"
+    DebugMsg 2 "no keys accepted"
     exit 1
 fi 
 
