12819aa224ddeb6ac8f37d75d62814019592d20a
Christian Knauer <email@christianknauer.de>
2023-09-16 20:58:44 +0200

    Work on ssh-agent encryption.

--- sa-crypt/sa-decrypt.sh
+++ sa-crypt/sa-decrypt.sh
@@ -43,10 +43,6 @@ DECFILE=$(mktemp -p $TEMPD)
 [ ! -e "$DECFILE" ] && ErrorMsg "failed to create temp dec file" && exit 1
 DebugMsg 3 "using \"$DECFILE\" as temp dec file"
 
-#TMDFILE=$(mktemp -p $TEMPD)
-#[ ! -e "$TMDFILE" ] && ErrorMsg "failed to create temp chk file" && exit 1
-#DebugMsg 3 "using \"$TMDFILE\" as temp chk file"
-
 # determine encryption key specification
 if sacrypt_DetermineKeyHash "${PUBKEYFILE}"; then
     KEYSPEC=$retval
@@ -71,46 +67,6 @@ if [ "${OUTFILE}" == "" ]; then
 else
     cp "${DECFILE}" "${OUTFILE}"
     DebugMsg 1 "output written to \"${OUTFILE}\""
-fi
-
-exit 0
-
-# EOF
-# find the encryption key in the agent 
-
-if sacrypt_FindKeyInAgent ${KEYSPEC}; then
-    KEYINDEX=$retval
-    KEYHASH=$retval1
-    DebugMsg 1 "key ${KEYSPEC} found in agent (#${KEYINDEX})"
-else
-    ErrorMsg "key ${KEYSPEC} not found in agent (#$retval)"; exit 1
-fi
-
-[ ! -e "$INFILE" ] && ErrorMsg "input file \"$INFILE\" cannot be opened" && exit 1
-cat "${INFILE}" | ${DECRYPT} > "${DECFILE}" ; ec=$?  
-case $ec in
-    0) DebugMsg 1 "decryption successful";;
-    1) ErrorMsg "decryption failed (key not in agent? not an sae file?)"; exit 1;;
-    *) ErrorMsg "decrypt gives unknown exit code ($ec)"; exit 2;;
-esac
-
-if [ "${OUTFILE}" == "" ]; then
-    cat "${DECFILE}" 
-    DebugMsg 1 "output sent to STDOUT"
-else
-    cp "${DECFILE}" "${OUTFILE}"
-    DebugMsg 1 "output written to \"${OUTFILE}\""
-fi
-
-if [ "${CHKFILE}" == "" ]; then
-    DebugMsg 1 "no checksum data available, verification skipped"
-else
-    sacrypt_ComputeHashOfFile "${DECFILE}" > "${TMDFILE}"
-    cmp -s "${CHKFILE}" "${TMDFILE}" ; ec=$?  
-    case $ec in
-        0) DebugMsg 1 "checksum verification passed";;
-        *) ErrorMsg "checksum verification failed" && exit 1;;
-    esac
 fi
 
 exit 0
