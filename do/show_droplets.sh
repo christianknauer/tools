4951254759ea7e3dcc58a9c3e61b1da1f15d0c3d
Christian Knauer <email@christianknauer.de>
2023-11-22 12:43:33 +0100

    lib update

--- do/show_droplets.sh
+++ do/show_droplets.sh
@@ -6,7 +6,7 @@ source lib.inc.sh
 [ -z "$LIB_DIRECTORY" ] && echo "show_droplets.sh: LIB_DIRECTORY not defined, terminating." && exit 1
 
 LOGGING_NAMESPACE="."
-source ${LIB_DIRECTORY}/logging.inc.sh
+source ${LIB_DIRECTORY}/logging.sh
 #LOGGING_DEBUG_LEVEL=3
 
 source token.inc.sh
@@ -15,6 +15,8 @@ InfoMsg "show_droplets"
 
 DROPLETS=$(curl -sS -X GET "https://api.digitalocean.com/v2/droplets" \
 	-H "Authorization: Bearer $DO_TOKEN")
+
+echo $DROPLETS
 
 DROPLETS=$(echo $DROPLETS | jq ".droplets")
 
