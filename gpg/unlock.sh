23ccc4ca125034dc1d7e3b9e8cc66adcfb8961ba
Christian Knauer <email@christianknauer.de>
2024-01-20 21:40:22 +0100

    gnupg

--- gpg/unlock.sh
+++ gpg/unlock.sh
@@ -4,7 +4,19 @@ Email=email@christianknauer.de
 
 gpg-connect-agent 'keyinfo --list' /bye
 
-echo "$(ph show @credentials/gpg/${Email} --field password)" | ${GPPCMD} --preset $(ph show @credentials/gpg/${Email} --field keygrip)
+fingerprints=($(gpg -K --fingerprint --with-colons ${Email} | sed -nr '/fpr/,+1{s/^grp:+(.*):$/\1/p}'))
+# preset each fingerprint
+for fingerprint in "${fingerprints[@]}"
+do
+    echo "$fingerprint"
+    echo deadbeefaffe66666666666666 | ${GPPCMD} --preset $fingerprint
+done
+
+gpg-connect-agent 'keyinfo --list' /bye
+
+return 0
+echo $(ph show @credentials/gpg/${Email} --field password) | ${GPPCMD} --preset $(ph show @credentials/gpg/${Email} --field keygrip)
+
 
 gpg-connect-agent 'keyinfo --list' /bye
 
