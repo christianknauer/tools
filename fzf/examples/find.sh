277fda69ad5d8f0073c5753cb2fe3e71a691f980
Christian Knauer <email@christianknauer.de>
2024-01-28 22:20:14 +0100

    fzf scripts

---
+++ fzf/examples/find.sh
@@ -0,0 +1,2 @@
+find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --scheme=path --prompt '> ' --border=rounded --preview 'fzf-preview {}' --preview-window 'right,40%,+{2}+3/3,~3' --height 90% --header-first --header 'ranger'
