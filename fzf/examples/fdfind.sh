cc2c60351b6f4e63f28a59903c9126b6cf2f78b1
Christian Knauer <email@christianknauer.de>
2024-01-28 22:20:35 +0100

    fzf scripts

--- fzf/examples/fdfind.sh
+++ fzf/examples/fdfind.sh
@@ -1,6 +1,6 @@
 fdfind --type f --strip-cwd-prefix |
-   fzf \
-   --prompt '\ > ' \
-   --bind 'ctrl-d:unbind(ctrl-d)+reload(fdfind --type d --strip-cwd-prefix)+transform-prompt(echo d\>\ )+rebind(ctrl-f)' \
-   --bind 'ctrl-f:unbind(ctrl-f)+reload(fdfind --type f --strip-cwd-prefix)+transform-prompt(echo \ \>\ )+rebind(ctrl-d)' \
-   +m --reverse --scheme=path --prompt '> ' --border=rounded --preview 'fzf-preview {}' --preview-window 'right,40%,+{2}+3/3,~3' --height 90% --header-first --header 'ranger'
+  fzf \
+    --prompt '\ > ' \
+    --bind 'ctrl-d:unbind(ctrl-d)+reload(fdfind --type d --strip-cwd-prefix)+transform-prompt(echo d\>\ )+rebind(ctrl-f)' \
+    --bind 'ctrl-f:unbind(ctrl-f)+reload(fdfind --type f --strip-cwd-prefix)+transform-prompt(echo \ \>\ )+rebind(ctrl-d)' \
+    +m --reverse --scheme=path --prompt '> ' --border=rounded --preview 'fzf-preview {}' --preview-window 'right,40%,+{2}+3/3,~3' --height 90% --header-first --header 'ranger'
--- fzf/examples/rg.sh
+++ fzf/examples/rg.sh
@@ -1,8 +1,8 @@
 : | rg_prefix='rg --column --line-number --no-heading --color=always --smart-case' \
-    fzf \
-        --bind 'start:reload:$rg_prefix ""' \
-        --bind 'change:reload:$rg_prefix {q} || true' \
-        --bind 'enter:become(echo {} | cut -d: -f1)' \
-        --color "hl:-1:underline,hl+:-1:underline:reverse" \
-        --ansi --disabled \
-   +m --reverse --scheme=path --prompt '> ' --border=rounded --preview 'fzf-preview {}' --preview-window 'right,40%,+{2}+3/3,~3' --height 90% --header-first --header 'ranger'
+  fzf \
+  --bind 'start:reload:$rg_prefix ""' \
+  --bind 'change:reload:$rg_prefix {q} || true' \
+  --bind 'enter:become(echo {} | cut -d: -f1)' \
+  --color "hl:-1:underline,hl+:-1:underline:reverse" \
+  --ansi --disabled \
+  +m --reverse --scheme=path --prompt '> ' --border=rounded --preview 'fzf-preview {}' --preview-window 'right,40%,+{2}+3/3,~3' --height 90% --header-first --header 'ranger'
