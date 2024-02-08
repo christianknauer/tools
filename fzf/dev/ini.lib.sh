c58bffeaebbe3926f893d3d35e52739f9311faae
Christian Knauer <email@christianknauer.de>
2024-02-08 01:12:12 +0100

    opts handling

--- fzf/dev/ini.lib.sh
+++ fzf/dev/ini.lib.sh
@@ -9,25 +9,60 @@
 # does not collide with a name higher up the
 # call chain.
 
-ini::serialize_array_to_string2() {
+ini::create_argname() {
+   local avoid="$1"
+   local base="$2"; [ -z "$base" ] && base="a${RANDOM}"
+   base="${base}_${#FUNCNAME[@]}_$$_"
+   local argname="${base}${RANDOM}"
+   while [ "$avoid" = "$argname" ]; do
+     argname="${base}${RANDOM}"
+   done
+   echo "$argname"
+}
 
-#   local _name="_name_$$$RANDOM"
-#   while [ "$1" = "$$$_name" ]; do
-#     _name="_name_$$$RANDOM"
-#   done
-#   declare -n arr="$_name"
-#   local -A ${!arr}="$1"
-#   declare -p ${!arr}
+ini::get_dict_from_dict() {
+  [ "$1" = "dict" ] || { local -n dict; dict="$1"; }
+  local key="$2"
+  [ "$3" = "out" ] || { local -n out; out="$3"; }
+  echo ${dict["$key"]}
+  out=${dict["$key"]}
+  return
 
-  ##WORKS
+  local -A res=${dict["$key"]}
+  declare -p res
+  #out=${dict["$key"]}
+
+  #local LIST="$(declare -p res 2>/dev/null)"
+  local LIST="${dict["$key"]}"
+  [[ "$LIST" ]] && out+=${LIST}
+
+  return
+  declare -A serialized=${dict["$key"]}
+  serialized="${serialized#declare -A *=}"
+  echo "${serialized}"
+  #declare -p serialized
+  #res="${serialized}"
+}
+
+ini::add_dict_to_dict() {
+  [ "$1" = "dict" ] || { local -n dict; dict="$1"; }
+  local key="$2"
+  [ "$3" = "val" ] || { local -n val; val="$3"; }
+ 
+  local serialized="$(declare -p "${!val}")"
+  serialized="${serialized#declare -A *=}"
+  dict["$key"]="$serialized"
+}
+
+# works 
+ini::serialize_array_to_string() {
   [ "$1" = "arr" ] || { local -n arr; arr="$1"; }
-  declare -p ${!arr}
 
   local depth=$2
   [ -z "$depth" ] && depth=0
  
   local indent="                                    "
-  indent=${indent:0:((depth*2))}
+  indent=${indent:0:((depth*4))}
   ((depth++))
 
   local res=""
@@ -37,17 +72,21 @@ ini::serialize_array_to_string2() {
     pkey="${pkey//=/\\\\=}"
     local pval="${val//\"/\\\\\"}"
     pval="${pval//=/\\\\=}"
-#    echo "\n$key/$pkey  --  $val/$pval"
-#    declare -p ${!arr}
+#    echo >&2 -e -n "\n$indent$key ------------ "
+#    echo >&2 $(declare -p ${!arr})
+#    echo >&2 -e "$indent]\n"
     if [[ "$val" =~ ^\((.*)\)$ ]]; then
       res+="${indent}[$pkey]\n"
 
-      declare -n data="data$$$depth$RANDOM"
+      #declare -n data="data$$$depth$RANDOM"
+      local -n data="$(ini::create_argname)"
       local -A ${!data}="${val}"
-#      declare -p ${!data}
-      res+="$(ini::serialize_array_to_string2 ${!data} $depth)"
-
+#      echo >&2 -e -n "\n$indent ------------ "
+#      echo >&2 $(declare -p ${!data})
+      res+="$(ini::serialize_array_to_string ${!data} $depth)"
       res+="${indent}[/$pkey]\n"
+      unset -n data
+      unset "${!data}"
     else
       res+="$indent\"$pkey\"=\"$pval\"\n"
     fi
@@ -55,7 +94,7 @@ ini::serialize_array_to_string2() {
   echo "$res"
 }
 
-ini::serialize_array_to_string() {
+ini::serialize_array_to_string_old() {
   [ "$1" = "arr" ] || { declare -n arr; arr="$1"; }
 
   local res=""
--- fzf/dev/initf.sh
+++ fzf/dev/initf.sh
@@ -43,6 +43,42 @@ declare -A options_cfg
 options_cfg[just a key]='i am just a value'
 options_cfg[secret]='([description]="secret option only for the ini file" [type]="string" [init]="topsecret" [modes]="([dumb]="ass" [top]="bottom" [deeper]="single" [x]=\"( [y]=\"1\" )\" )" [one more]="meaningless")'
 options_cfg[envonly]='([description]="option only for env vars file" [type]="int" [init]="" [modes]="e")'
+#options_cfg[secretcopy]='([description]="secret option only for the ini file" [type]="string" [init]="topsecret" [modes]="([dumb]="ass" [top]="bottom" [deeper]="single" [x]=\"( [y]=\"1\" )\" )" [one more]="meaningless")'
 declare -p options_cfg
-echo -e "$(ini::serialize_array_to_string2 options_cfg)"
 
+echo -e -n "$(ini::serialize_array_to_string options_cfg)"
+
+declare -A dall
+
+declare -A dict1; dict1[a1]="a1"; dict1[b1]="b1"
+declare -A dict2; dict2[a2]="a2"; dict2[b2]="b2"
+declare -A dict3; dict3[a3]="a3"; dict3[b3]="b3"
+ini::add_dict_to_dict dict2 'c2' dict3
+ini::add_dict_to_dict dict1 'c1' dict2
+
+declare -A fdict
+fdict[a]="a"
+fdict[b]="b"
+
+declare -A sdict
+sdict[x]="x"
+sdict[y]="y"
+
+#declare -p fdict
+#declare -p sdict
+
+ini::add_dict_to_dict sdict 'xxx' dict1
+ini::add_dict_to_dict sdict 'xxx2' dict1
+
+ini::add_dict_to_dict fdict '2nd' sdict
+#declare -p fdict
+
+ini::add_dict_to_dict dall 'first' fdict
+#declare -p dall
+
+declare -A resu
+ini::get_dict_from_dict dall 'first' resu
+declare -p resu
+
+echo -e -n "$(ini::serialize_array_to_string res)"
+#echo -e -n "$(ini::serialize_array_to_string dall)"
